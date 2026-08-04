da18a4ab2c27ca0474ecb088a3a9d9def6ed8d57
Rishi Kadam <rk5570376@gmail.com>
2026-08-05 00:06:44 +0530

    Changes in files

--- conftest.py
+++ conftest.py
@@ -1,10 +1,18 @@
-import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pytest
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    options = Options()
+
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
+    driver.set_window_size(1920, 1080)
     driver.get("https://www.saucedemo.com/")
 
     yield driver
